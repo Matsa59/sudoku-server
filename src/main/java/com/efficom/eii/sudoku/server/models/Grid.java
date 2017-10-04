a1c694277c5c2735417a5684d68dc42037bdeebe
Alexandre <alexandre.lprtr@gmail.com>
2017-10-04 14:53:05 +0200

    Fix grid

--- src/main/java/com/efficom/eii/sudoku/server/models/Grid.java
+++ src/main/java/com/efficom/eii/sudoku/server/models/Grid.java
@@ -10,6 +10,9 @@ public class Grid {
 
     public Grid() {
         sections = new Section[9];
+        for(Section section : sections) {
+            section = new Section();
+        }
     }
 
     public boolean isValid() {
